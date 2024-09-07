f8515df654f062b0d55e819a683ff2af615115e8
Praveen172 <prasannamoorthy172@gmail.com>
2024-09-07 00:21:39 -0400

    webapp cmp

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -4,19 +4,29 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  swcMinify: true, 
+  swcMinify: true,
   experimental: {
-    appDir: true,   
-    scrollRestoration: true, 
+    appDir: true,
+    scrollRestoration: true,
   },
-  compress: true,   
+  compress: true,
   webpack(config, { isServer }) {
     if (!isServer) {
+      config.cache = {
+        type: 'filesystem', 
+        buildDependencies: {
+          config: [__filename], 
+        },
+        compression: 'brotli', 
+        maxAge: 1000 * 60 * 60 * 24 * 7, 
+      };
+
       config.resolve.fallback = {
-        ...config.resolve.fallback, 
-        fs: false, 
+        ...config.resolve.fallback,
+        fs: false,
       };
     }
     return config;
   },
 });
+
